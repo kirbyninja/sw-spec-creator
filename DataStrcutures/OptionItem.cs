f77fdf77985de2ecbd304512236f1c19d4ec3b1f
Kirby Liu <kirbyninja@users.noreply.github.com>
2017-02-14 20:56:53 +0800

    加入暫存資料自定義型別

---
+++ DataStrcutures/OptionItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecCreator.DataStrcutures
+{
+    public class OptionItem
+    {
+        public OptionItem()
+        { }
+
+        public OptionItem(int optionNo, int itemNo, string text)
+            : this()
+        {
+            this.OptionNo = optionNo;
+            this.ItemNo = itemNo;
+            this.Text = text;
+        }
+
+        public int ItemNo { get; set; }
+        public int OptionNo { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ DataStrcutures/WorkingTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecCreator.DataStrcutures
+{
+    public class WorkingTable
+    {
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string TableName { get; set; }
+        public IEnumerable<WorkingColumn> WorkingColumns { get; set; }
+    }
+}
