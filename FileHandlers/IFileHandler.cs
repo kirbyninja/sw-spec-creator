a9290f0fc23574e2c62bd9e1bbb71cde99f83262
Kirby Liu <kirbyninja@users.noreply.github.com>
2017-02-14 20:57:37 +0800

    新增 IFileHandler以及其泛型介面

---
+++ FileHandlers/IFileHandler.cs
@@ -0,0 +1,30 @@
+﻿using SpecCreator.DataStrcutures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecCreator.FileHandlers
+{
+    public interface IFileHandler
+    {
+        string Description { get; }
+        string Extension { get; }
+
+        WorkingTable Load(string fileName);
+
+        void Save(WorkingTable table, string fileName);
+    }
+
+    public interface IFileHandler<T> : IFileHandler
+    {
+        new T Load(string fileName);
+
+        void Save(T meta, string fileName);
+
+        T ConvertToMeta(WorkingTable table);
+
+        WorkingTable ConvertToTable(T meta);
+    }
+}
